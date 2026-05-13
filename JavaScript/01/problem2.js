779e036ef42fcc8681b59ca2837fa3d5799c4d11
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-13 23:16:18 +0600

    solve average score problem

---
+++ JavaScript/01/problem2.js
@@ -0,0 +1,13 @@
+// You have 3 different exams with marks 75, 80, 90. Check if the average score is at least 80.
+
+const marks1 = 75;
+const marks2 = 83;
+const marks3 = 33;
+
+let average = (marks1 + marks2 + marks3) / 3;
+
+if (average >= 80) {
+  console.log("Average is at least 80");
+} else {
+  console.log("Average is less then 80");
+}
