7aa1d4488802957f526d18ce75bc0f6cee57f894
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-13 22:49:42 +0600

    start a new javascript journey

---
+++ JavaScript/01/problem1.js
@@ -0,0 +1,8 @@
+// You have 20 sweets. You gave 5 to your friend. Now, write a program to find out how many sweets you have left.
+
+const mySweets = 20;
+const givenSweets = 5;
+
+const remainingSweets = mySweets - givenSweets;
+
+console.log(remainingSweets);
