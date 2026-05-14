fd3950d6f43c2c6d32590ec25dc89ec15da67c3e
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-14 22:10:32 +0600

    practice JavaScript object creation

---
+++ JavaScript/01/problem3.js
@@ -0,0 +1,10 @@
+// Create an object containing information about your favorite book, including the book’s name and the author’s name. For example: `{ name: 'Sonar Tori', writer: 'Rabindranath Tagore' }`. Now, print the object to the console and see what the name of the book is.
+
+
+
+let favoriteBook = {
+  name: "Sonar Tori",
+  writer: "Rabindranath Tagore",
+};
+
+console.log("Favorite book name:", favoriteBook.name);
