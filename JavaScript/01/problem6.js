7a4e645449ce03ba7a914e734a32131081c47485
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-17 23:06:01 +0600

    added object practice problem for phone details

---
+++ JavaScript/01/problem6.js
@@ -0,0 +1,9 @@
+// You have a phone. Create an object that stores the brand, model, and price of the phone. After creating the object, display the price of the phone in the console.
+
+let phone = {
+    brand: "Honor",
+    model: "Honor 600",
+    price: "40000",
+};
+
+console.log("Phone Price:", phone.price);
