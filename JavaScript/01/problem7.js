04050793b1dff48d9bd545ede6978fb5cae7df3a
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-17 23:13:26 +0600

    array indexing exercise

---
+++ JavaScript/01/problem7.js
@@ -0,0 +1,6 @@
+// Create an array containing the names of 3 friends, for example: Alim, Hakim, Kalim. Then print the name of the first friend to the console.
+
+let friends = ["Alim", "Hakim", "Kalim"];
+
+console.log("First friend:", friends[0]);
+
