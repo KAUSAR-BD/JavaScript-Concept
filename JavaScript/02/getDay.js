6d423cfc4720002d10ba08a35558a7d0ecade935
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-20 07:02:43 +0600

    added folder 02 and create current day problem

---
+++ JavaScript/02/getDay.js
@@ -0,0 +1,10 @@
+// current date 
+
+let today = new Date(); // it creates an object containing current date and time.
+
+let day = today.getDay(); // it returns weekday as a number.
+
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
+console.log("Today is:"+ " " + daylist[day]);
+
