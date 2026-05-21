5ba36247740f5087caf9fb31f738724921da46eb
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-21 06:51:33 +0600

    solve basic algebra problem in JS

---
+++ JavaScript/02/number.js
@@ -0,0 +1,12 @@
+// A number is multiplied by 3 and then 5 is added. The result is 26. What is the number?
+
+//3x + 5 = 26
+// 3x = 26 - 5
+// 3x = 21
+// x = 21/3
+// x = 7
+
+let result = 26;
+let x = (result - 5) / 3;
+
+console.log(x)
