d997bffee99d6352728f3e695b754cb8af8e02ff
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-22 23:53:29 +0600

    solve basic linear equation

---
+++ JavaScript/02/linearEquation1.js
@@ -0,0 +1,13 @@
+// Twice a number minus 9 equals 25. What is the number?
+
+// 2x - 9 = 25
+// 2x = 25 + 9
+// 2x = 34
+// x = 34 / 2
+// x = 17
+
+
+let result = 25;
+let num = (result + 9) / 2;
+
+console.log(num);
