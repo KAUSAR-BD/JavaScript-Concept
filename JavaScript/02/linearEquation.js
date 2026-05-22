35a705665cce5e044f1f371fb0569ad6032ea72d
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-22 23:19:38 +0600

    solve basic linear equation

---
+++ JavaScript/02/linearEquation.js
@@ -0,0 +1,13 @@
+// A number is divided by 4 and then 6 is added. The result is 14. What is the number?
+
+// x/4 + 6 = 14
+// x/4 = 14-6
+// x/4 = 8
+// x = 8 * 4
+// x = 32
+
+
+let result = 14;
+let num = (result - 6) * 4;
+
+console.log(num);
