26b9034d831dae2666ab8e1b38adbfb58515eec2
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-05-24 23:37:49 +0600

    solve with Double equal

---
+++ JavaScript/01/problem8.js
@@ -0,0 +1,12 @@
+// Check if 5 ans '5' are equal using double equal.
+
+
+
+const number1 = 5;
+const str1 = "5";
+
+if (number1 == str1) {
+  console.log('5 and "5" are equal using double equal');
+} else {
+  console.log('5 and "5"are not equal');
+}
