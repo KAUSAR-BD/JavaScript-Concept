8137b7bf70e03a2157d48032bf439f3560664614
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-06-01 19:27:00 +0600

    train distance calculation

---
+++ JavaScript/01/problem9.js
@@ -0,0 +1,10 @@
+// If a train travels 180 km in 3 hours, how far will it travel in 7 hours at the same speed?
+
+
+let distance = 180;
+let time = 3;
+
+let speed = distance / time; // km per hour
+let distanceIn7Hours = speed * 7;
+
+console.log(distanceIn7Hours);
